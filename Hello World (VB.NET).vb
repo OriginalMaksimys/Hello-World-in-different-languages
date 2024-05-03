a757a297ba3cab9080d8ee33176046fcbde4fb9b
Maksimys_ <139385610+OriginalMaksimys@users.noreply.github.com>
2024-05-03 20:15:58 +0400

    I think someone will need this

---
+++ Hello World (VB.NET).vb
@@ -0,0 +1,15 @@
+Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Text
+Imports System.Threading.Tasks
+
+Namespace "Hello World"
+    Friend Class Program
+        Shared Sub Main(args As String())
+            Console.WriteLine("Hello World")
+            Console.ReadKey()
+        End Sub
+    End Class
+End Namespace
+
